b979ed98936005e497245ab483e55165208c914f
unknown <ludovic.spina@icloud.com>
2023-01-17 17:30:20 +0100

    fetch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {Client, Intents} = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
-
+import fetch from 'node-fetch';
 
 const token = 'MTA2NDkxODM2NDYwNDA4NDIyNQ.G47VtM.HXTWg5gClPzYT8OpB-SV0QKTYWzNITdDJK0zY8'
 
