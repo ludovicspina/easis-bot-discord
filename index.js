628ce9e440cb584b0f07ad12722e9dac9837cd9d
unknown <ludovic.spina@icloud.com>
2023-01-17 23:37:56 +0100

    update graph

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const {Client, Intents} = require('discord.js');
+const {Client, Intents, MessageEmbed} = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
-
+const lib = require('lib');
 const token = ''
 
 client.once('ready', () => {
@@ -8,36 +8,41 @@ client.once('ready', () => {
 });
 
 client.on("messageCreate", message => {
-    if (message.content === "$red cd") {
+    if (message.content === "$red ready") {
         fetch("https://opensheet.elk.sh/1Om7FhiE9yNgUN7PLGJ0f9Xqux7x03qP4tUBYe751qP0/CD%20RedM")
             .then((response) => response.json())
             .then((data) => {
-                let tabConstruit = [];
-                data.forEach(player => {
-                    if (player.etat_du_cooldown !== "Nouveau joueur") {
-                        tabConstruit += (player.participants_inscrits + ' ' + player.etat_du_cooldown + "\n")
-                    }
-                })
-                message.channel.send(tabConstruit)
-            })
-            .catch((error) => console.log("error"));
-    }
-})
-
-client.on("messageCreate", message => {
-    if (message.content === "$red dispo") {
-        fetch("https://opensheet.elk.sh/1Om7FhiE9yNgUN7PLGJ0f9Xqux7x03qP4tUBYe751qP0/CD%20RedM")
-            .then((response) => response.json())
-            .then((data) => {
-                let tabConstruit = [];
+                let TANK = [];
+                let DPS = [];
+                let HEAL = [];
+                let HELP = [];
                 data.forEach(player => {
                     if (player.etat_du_cooldown !== "Nouveau joueur") {
                         if (player.etat_du_cooldown === "CD terminé") {
-                            tabConstruit += (player.participants_inscrits + ' ' + player.etat_du_cooldown + "\n")
+                            if (player.role === "Tank") {
+                                TANK += (player.participants_inscrits + " => " + ":white_check_mark:" + "\n")
+                            } else if (player.role === "DPS" || player.role === "Low DPS") {
+                                DPS += (player.participants_inscrits + " => " + ":white_check_mark:" + "\n")
+                            } else if (player.role === "Prêtre principal") {
+                                HEAL += (player.participants_inscrits + " => " + ":white_check_mark:" + "\n")
+                            } else if (player.role === "Prêtre rez") {
+                                HELP += (player.participants_inscrits + " => " + ":white_check_mark:" + "\n")
+                            }
                         }
                     }
                 })
-                message.channel.send(tabConstruit)
+                message.channel.send({
+                    embeds: [{
+                        type: "rich",
+                        title: "Liste des cooldowns terminés",
+                        color: 0x890303,
+                        description: "**__Tanks :__**\n" + TANK + "\n" + "**__Dps :__**\n" + DPS + "\n" + "**__Main healer :__**\n" + HEAL + "\n" + "**__Help healer :__**\n" + HELP,
+                        footer: {
+                            text: "Bot by @pamp1n / Google Docs by @Caldera"
+                        },
+                        url: "https://docs.google.com/spreadsheets/d/1Om7FhiE9yNgUN7PLGJ0f9Xqux7x03qP4tUBYe751qP0/edit#gid=0"
+                    }]
+                });
             })
             .catch((error) => console.log("error"));
     }
@@ -48,19 +53,42 @@ client.on("messageCreate", message => {
         fetch("https://opensheet.elk.sh/1Om7FhiE9yNgUN7PLGJ0f9Xqux7x03qP4tUBYe751qP0/CD%20RedM")
             .then((response) => response.json())
             .then((data) => {
-                let tabConstruit = [];
+                let TANK = [];
+                let DPS = [];
+                let HEAL = [];
+                let HELP = [];
                 data.forEach(player => {
                     if (player.etat_du_cooldown !== "Nouveau joueur") {
                         if (player.etat_du_cooldown !== "CD terminé") {
-                            tabConstruit += (player.participants_inscrits + ' ' + player.etat_du_cooldown + "\n")
+                            if (player.role === "Tank") {
+                                TANK += (player.participants_inscrits + " => " + player.etat_du_cooldown + "\n")
+                            } else if (player.role === "DPS" || player.role === "Low DPS") {
+                                DPS += (player.participants_inscrits + " => " + player.etat_du_cooldown + "\n")
+                            } else if (player.role === "Prêtre principal") {
+                                HEAL += (player.participants_inscrits + " => " + player.etat_du_cooldown + "\n")
+                            } else if (player.role === "Prêtre rez") {
+                                HELP += (player.participants_inscrits + " => " + player.etat_du_cooldown + "\n")
+                            }
                         }
                     }
                 })
-                message.channel.send(tabConstruit)
+                message.channel.send({
+                    embeds: [{
+                        type: "rich",
+                        title: "Liste des cooldowns cours",
+                        color: 0x890303,
+                        description: "**__Tanks :__**\n" + TANK + "\n" + "**__Dps :__**\n" + DPS + "\n" + "**__Main healer :__**\n" + HEAL + "\n" + "**__Help healer :__**\n" + HELP,
+                        footer: {
+                            text: "Bot by @pamp1n / Google Docs by @Caldera"
+                        },
+                        url: "https://docs.google.com/spreadsheets/d/1Om7FhiE9yNgUN7PLGJ0f9Xqux7x03qP4tUBYe751qP0/edit#gid=0"
+                    }]
+                });
             })
             .catch((error) => console.log("error"));
     }
 })
+
 
 client.login(token);
 
