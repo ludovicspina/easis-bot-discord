321914ae2910e69a3cdae1a7e2a4b1b854cd3d04
unknown <ludovic.spina@icloud.com>
2023-01-29 15:33:28 +0100

    adding to form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {Client, Intents, MessageEmbed} = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 const lib = require('lib');
+// const fetch = require("node-fetch");
 const token = ''
 
 client.once('ready', () => {
@@ -90,17 +91,52 @@ client.on("messageCreate", message => {
 })
 
 
+
+client.on("messageCreate", message => {
+    if (message.content.includes("$red add")) {
+        let joueurs = [];
+        let date = "";
+        let heure = "";
+        let loot = "";
+
+        let s1 = message.content.split("$red add")
+        let s2 = s1[1].split(" ")
+        joueurs = s2[1].split(",")
+        date = s2[2].split("/")
+        let year = date[2]
+        let month = date[1]
+        let day = date[0]
+        heure = s2[3].split(",")
+        loot = s2[4].split(",")
+
+        let url = "https://docs.google.com/forms/d/e/1FAIpQLScCSVDl4rBYF011AACuvY399N4JbigIErLJQR9VCmjwsL6aBg/viewform?usp=pp_url&entry.522514556=" + "20" + year + "-" + month + "-" + day + "+" + heure
+
+        joueurs.forEach(joueur => {
+            url += "&entry.1769259102=" + joueur
+        })
+
+        url += "&entry.656140026=" + loot
+
+
+
+        message.channel.send({
+            embeds: [{
+                type: "rich",
+                title: "Lien généré",
+                color: 0x890303,
+                description: "" + url,
+                footer: {
+                    text: "Bot by @pamp1n / Google Docs by @Caldera"
+                },
+                url: "https://docs.google.com/spreadsheets/d/1Om7FhiE9yNgUN7PLGJ0f9Xqux7x03qP4tUBYe751qP0/edit#gid=0"
+            }]
+        });
+    }
+})
+
+
+
 client.login(token);
-
-
-
-
-
-
-
-
-
-
 
 
 
